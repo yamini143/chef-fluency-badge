580a9e9de998e92ecdfe218edfad3af6a40077f4
yamini143 <kondapaturiyaminipriya@gmail.com>
2017-10-14 05:31:26 +0000

    updatind default.rb file

--- cookbooks/apache1/recipes/default.rb
+++ cookbooks/apache1/recipes/default.rb
@@ -2,4 +2,15 @@
 # Cookbook:: apache1
 # Recipe:: default
 #
+#
 # Copyright:: 2017, The Authors, All Rights Reserved.
+#
+package 'apache2' do
+package_name 'httpd'
+action :install
+end
+
+service 'apache2' do
+service_name 'http'
+action [:enable, :start]
+end
