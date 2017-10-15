5056004aacd0d666afe474d414e80e0ce1d0e2cb
yamini143 <kondapaturiyaminipriya@gmail.com>
2017-10-15 09:45:29 +0000

    adding motd file and changes made in default file

--- cookbooks/apache1/recipes/default.rb
+++ cookbooks/apache1/recipes/default.rb
@@ -3,14 +3,21 @@
 # Recipe:: default
 #
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
-#
-package 'apache2' do
-package_name 'httpd'
-action :install
+# Copyright:: 2017, The Authors, All Rights Reserve
+
+	if node['platform_family'] == "rhel"
+		package = "httpd"
+	elsif node['platform_family'] == "debian"
+		package = "apache2"
+	end
+
+package 'apache' do
+ package_name package
+ action :install
 end
 
-service 'apache2' do
-service_name 'http'
+service 'apache1' do
+service_name 'httpd'
 action [:enable, :start]
 end
+
---
+++ cookbooks/apache1/recipes/motd.rb
@@ -0,0 +1,5 @@
+	
+	hostname = node["hostname"]
+ 	  file '/etc/motd' do
+		content "hostname is this:#(hostname)"
+	end
